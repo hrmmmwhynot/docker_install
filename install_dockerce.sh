1d01c136e2e278f48ceaf9df47217c0983189052
hrmmmwhynot <hrmmmwhynot@example.com>
2018-01-07 18:10:46 -0800

    made the install docker script more robust

--- install_dockerce.sh
+++ install_dockerce.sh
@@ -1,45 +1,76 @@
 #!/bin/bash
 
-echo "Y" | apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
+install_prompt() {
+  echo "Do you want to use devicemapper instead of auf for docker (put No if you don't know)?"
+  select choice in "Yes" "No"; do
+    case $choice in
+      Yes ) printf "\nInstalling devicemapper configs...\n" ;ubuntu_devicemapper_config; break;;
+      No ) printf "\nSkipping devicemapper configs...\n"; break;;
+    esac
+  done
+}
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+ubuntu_install_docker() {
+  echo "Y" | apt-get install \
+      apt-transport-https \
+      ca-certificates \
+      curl \
+      software-properties-common
+  
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  
+  apt-key fingerprint 0EBFCD88
+  
+  add-apt-repository \
+     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+     $(lsb_release -cs) \
+     stable"
+  
+  echo "Y" | apt-get update
+  
+  echo "Y" | apt-get install vim
+  
+  echo "Y" | apt-get install docker-ce
+  
+  echo "Y" | apt-get install docker-ce=17.12.0~ce-0~ubuntu
+  
+  install_prompt
+  service docker restart
 
-apt-key fingerprint 0EBFCD88
+  docker run hello-world
+}
 
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-
-echo "Y" | apt-get update
-
-echo "Y" | apt-get install vim
-
-echo "Y" | apt-get install docker-ce
-
-echo "Y" | apt-get install docker-ce=17.12.0~ce-0~ubuntu
-
-CUR_DIR=`pwd`
-MYDOCKER_CFGS="${CUR_DIR}/docker_configs"
-MYDOCKER_STORE="$CUR_DIR/docker_store"
-MYDOCKER_TMP=$MYDOCKER_STORE
-
-mkdir -p $CUR_DIR/docker_store
-docker_configs=$(cat <<EOH
+ubuntu_devicemapper_config() {
+  CUR_DIR=`pwd`
+  MYDOCKER_CFGS="${CUR_DIR}/docker_configs"
+  MYDOCKER_STORE="$CUR_DIR/docker_store"
+  MYDOCKER_TMP=$MYDOCKER_STORE
+  
+  mkdir -p $CUR_DIR/docker_store
+  docker_configs=$(cat <<-EOH
 export DOCKER_TMPDIR="$MYDOCKER_TMP"
 DOCKER_OPTS="--storage-driver=devicemapper -g $MYDOCKER_STORE"
 EOH
 )
+  echo "$docker_configs" > "${MYDOCKER_CFGS}/docker"
+  
+  cp -a /etc/docker/daemon.json /etc/docker/daemon.json.bak
+  cp -a /etc/default/docker /etc/default/docker.bak
+  cp -a "${MYDOCKER_CFGS}/daemon.json" /etc/docker/daemon.json
+  cp -a "${MYDOCKER_CFGS}/docker" /etc/default/docker
 
-cp -a "${MYDOCKER_CFGS}/daemon.json" /etc/docker/daemon.json
-cp -a "${MYDOCKER_CFGS}/docker" /etc/default/docker
+}
 
-service docker restart
+# check OS
+checkos() {
+  if ! cat /proc/version | grep -i ubuntu; then
+    printf "\nNot Ubuntu OS. Exiting...\n"
+    exit 1
+  elif `lsb_release -r | grep -e 16.04 > /dev/null 2>&1`;then
+    printf "\nUbuntu 16.04 is installed.. continuing\n"
+    ubuntu_install_docker
+  fi
+}
 
-docker run hello-world
-
+checkos
 
